0d03a3413985840cf7da4010eed4994128ce840a
adhindasalsabilla <126484597+adhindasalsabilla@users.noreply.github.com>
2024-06-18 15:29:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import mysql.connector
 from sqlalchemy import create_engine
 import numpy as np
 
